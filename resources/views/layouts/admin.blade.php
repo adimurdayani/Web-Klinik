aeb0e95bc58082a9a24dd4224d3cdfdc784eb69a
adimurdayani <adimurdayani@gmail.com>
2024-10-14 11:49:21 +0800

    *update: template

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -66,7 +66,7 @@
                 });
             }
         });
-    </script> <!--end::OverlayScrollbars Configure--> <!-- OPTIONAL SCRIPTS --> <!-- sortablejs -->
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"
         integrity="sha256-ipiJrswvAR4VAx/th+6zWsdeYmVae0iJuiR+6OqHJHQ=" crossorigin="anonymous"></script> <!-- sortablejs -->
 
